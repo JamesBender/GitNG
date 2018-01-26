843b96db5291fd31aeb1966606c2215646054857
James Bender <James@JamesCBender.com>
2018-01-25 22:48:50 -0500

    Added user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GithubUserComponent } from './github-user/github-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubUserComponent
   ],
   imports: [
     BrowserModule
